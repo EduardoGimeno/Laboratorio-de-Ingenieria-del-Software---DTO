5c13d4f3363b3dde422d8b0e99efc0f2bc7008d6
Gonzalo Berne Melguizo <gera1397@gmail.com>
2020-05-31 21:23:12 +0200

    BusquedaDTO actualizado con fechas y horas

--- src/main/java/DTO/BusquedaDTO.java
+++ src/main/java/DTO/BusquedaDTO.java
@@ -1,13 +1,21 @@
 package DTO;
 
+import Enum.Dia;
 import java.io.Serializable;
 import java.util.List;
+import java.sql.Timestamp;
 
 public class BusquedaDTO implements Serializable {
     private String edificio;
     private String tipoEspacio;
     private List<EquipamientoDTO> equipamiento;
     private int capacidad;
+    private Timestamp fechaInicio;
+    private Timestamp fechaFin;
+    private Integer horaInicio;
+    private Integer horaFin;
+    private List<Dia> dias;
+    private boolean periodo;
 
     public String getEdificio() {
         return edificio;
@@ -39,5 +47,53 @@ public class BusquedaDTO implements Serializable {
 
     public void setEquipamiento(List<EquipamientoDTO> equipamiento) {
         this.equipamiento = equipamiento;
+    }
+
+    public Timestamp getFechaInicio() {
+        return fechaInicio;
+    }
+
+    public void setFechaInicio(Timestamp fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    public Timestamp getFechaFin() {
+        return fechaFin;
+    }
+
+    public void setFechaFin(Timestamp fechaFin) {
+        this.fechaFin = fechaFin;
+    }
+
+    public Integer getHoraInicio() {
+        return horaInicio;
+    }
+
+    public void setHoraInicio(Integer horaInicio) {
+        this.horaInicio = horaInicio;
+    }
+
+    public Integer getHoraFin() {
+        return horaFin;
+    }
+
+    public void setHoraFin(Integer horaFin) {
+        this.horaFin = horaFin;
+    }
+
+    public List<Enum.Dia> getDias() {
+        return dias;
+    }
+
+    public void setDias(List<Enum.Dia> dias) {
+        this.dias = dias;
+    }
+
+    public boolean isPeriodo() {
+        return periodo;
+    }
+
+    public void setPeriodo(boolean periodo) {
+        this.periodo = periodo;
     }
 }
